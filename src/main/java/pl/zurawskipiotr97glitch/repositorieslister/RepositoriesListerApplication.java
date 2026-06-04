e14485297a64340044497215430e7420f0babbfe
zurawskipiotr97-glitch <zurawskipiotr97@gmail.com>
2026-06-04 21:16:04 +0200

    Origin

---
+++ src/main/java/pl/zurawskipiotr97glitch/repositorieslister/RepositoriesListerApplication.java
@@ -0,0 +1,13 @@
+package pl.zurawskipiotr97glitch.repositorieslister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoriesListerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepositoriesListerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/zurawskipiotr97glitch/repositorieslister/RepositoriesListerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zurawskipiotr97glitch.repositorieslister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoriesListerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
